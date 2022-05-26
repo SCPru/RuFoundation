42531380e1e57b3e03f01154f13f064a4683cd5b
mrnereof <mrnereof@gmail.com>
2022-05-26 23:00:18 +0500

    Reorganization of urlconfig

--- web/urls.py
+++ web/urls.py
@@ -13,27 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, re_path
 from django.views.decorators.csrf import csrf_exempt
 
-import django.views.static
+import web.views.article
 
-
-def partial_quote(url):
-    return url.replace(':', '%3A')
-
-
-def serve_static(request, path, document_root=None, show_indexes=False):
-    path = '/'.join([partial_quote(x) for x in path.split('/')])
-    return django.views.static.serve(request, path, document_root=document_root, show_indexes=show_indexes)
+import web.views.api.articles
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    re_path(r'^static/(?P<path>.*$)', serve_static, {'document_root': './web/static'}),
-    re_path(r'^local--files/(?P<path>.*)$', serve_static, {'document_root': './files'}),
-
-    path("", include("web.urls"))
+    path('api/articles/new', csrf_exempt(web.views.api.articles.create)),
+    path('api/articles/<str:full_name>', csrf_exempt(web.views.api.articles.fetch_or_update)),
+    path('api/articles/<str:full_name>/log', csrf_exempt(web.views.api.articles.fetch_log)),
+    re_path(r'(?P<path>.*)$', web.views.article.index)
 ]
