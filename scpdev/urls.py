975cfc89fb4d5bf2afb851329f0558e4afc436d8
mrnereof <mrnereof@gmail.com>
2022-05-30 16:47:50 +0500

    Return media files handler

--- scpdev/urls.py
+++ scpdev/urls.py
@@ -13,14 +13,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
-from django.conf.urls.static import static
-from django.contrib import admin
+from django.views.decorators.csrf import csrf_exempt
+from django.urls import path, re_path, include
 from django.conf import settings
+from django.contrib import admin
+
+import django.views.static
+
+
+def partial_quote(url):
+    return url.replace(':', '%3A')
+
+
+def serve_static(request, dir_path, document_root=None, show_indexes=False):
+    dir_path = '/'.join([partial_quote(x) for x in dir_path.split('/')])
+    return django.views.static.serve(request, dir_path, document_root=document_root, show_indexes=show_indexes)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    re_path(r'^'+settings.MEDIA_URL+r'/(?P<path>.*)$', serve_static, {'document_root': settings.MEDIA_ROOT}),
+
     path("", include("web.urls"))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
