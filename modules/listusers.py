a189bdda0bea96eca140fd034f1d8b18c4667818
ZZYZX <zzyzx@virtual>
2022-06-06 22:54:53 +0300

    Include userid in path

--- modules/listusers.py
+++ modules/listusers.py
@@ -25,8 +25,8 @@ def render_user_to_html(user, avatar=True):
         return ret
     ret = '<span class="printuser w-user" data-user-name="%s">' % html.escape(user.username)
     if avatar:
-        ret += '<a href="/-/users/%s"><img class="small" src="/static/images/default_avatar.png" alt="%s"></a>' % (urllib.parse.quote_plus(user.username), html.escape(user.username))
-    ret += '<a href="/-/users/%s">%s</a>' % (urllib.parse.quote_plus(user.username), html.escape(user.username))
+        ret += '<a href="/-/users/%d-%s"><img class="small" src="/static/images/default_avatar.png" alt="%s"></a>' % (urllib.parse.quote_plus(user.username), user.id, html.escape(user.username))
+    ret += '<a href="/-/users/%d-%s">%s</a>' % (urllib.parse.quote_plus(user.username), user.id, html.escape(user.username))
     ret += '</span>'
     return ret
 
@@ -36,7 +36,7 @@ def render_user_to_json(user, avatar=True):
         return {'type': 'system'}
     if isinstance(user, AnonymousUser):
         return {'type': 'anonymous', 'avatar': '/static/images/anon_avatar.png', 'name': 'Anonymous User', 'username': None, 'showAvatar': avatar}
-    return {'type': 'user', 'avatar': '/static/images/default_avatar.png', 'name': user.username, 'username': user.username, 'showAvatar': avatar}
+    return {'type': 'user', 'id': user.id, 'avatar': '/static/images/default_avatar.png', 'name': user.username, 'username': user.username, 'showAvatar': avatar}
 
 
 def has_content():
