427cb39104334d4f2b4f9a3fc982e4404dd75964
ZZYZX <zzyzx@virtual>
2022-06-06 22:47:14 +0300

    Add module listusers

--- modules/listusers.py
+++ modules/listusers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AnonymousUser
 import urllib.parse
 from django.utils import html
+import re
+import renderer
 
 
 def render_user_to_text(user):
@@ -35,3 +37,25 @@ def render_user_to_json(user, avatar=True):
     if isinstance(user, AnonymousUser):
         return {'type': 'anonymous', 'avatar': '/static/images/anon_avatar.png', 'name': 'Anonymous User', 'username': None, 'showAvatar': avatar}
     return {'type': 'user', 'avatar': '/static/images/default_avatar.png', 'name': user.username, 'username': user.username, 'showAvatar': avatar}
+
+
+def has_content():
+    return True
+
+
+def render(context, params, content=None):
+    # all params are ignored. always current user
+    if not context.user.is_authenticated:
+        return ''
+
+    tpl_vars = {
+        'number': str(context.user.id),
+        'title': context.user.username,
+        'name': context.user.username
+    }
+
+    template = (content or '').strip()
+    template = re.sub(r'(%%(.*?)%%)', lambda var: tpl_vars[var[2]] if var[2] in tpl_vars else '%%'+var[2]+'%%', template)
+
+    return renderer.single_pass_render(template, context)
+
