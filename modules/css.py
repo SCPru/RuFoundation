76e5ca5e64dc3abc9c84b41aa44a8de12caabbae
ZZYZX <zzyzx@virtual>
2022-06-20 15:19:18 +0300

    Fix XSS in module CSS

--- modules/css.py
+++ modules/css.py
@@ -1,10 +1,12 @@
+import re
+
+
 def has_content():
     return True
 
 
 def render(_context, _params, content=''):
     print('module css')
-    code = content \
-        .replace('\u00a0', ' ') \
-        .replace('</style>', '\\u003c/style\\u003e')
+    code = content.replace('\u00a0', ' ')
+    code = re.sub(r'<\s*/\s*style\s*>', '\\\\u003c/style\\\\u003e', code)
     return '<style>%s</style>' % code
