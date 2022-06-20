6e3acf75c71a89b0bbe57b5a22a56abe652d2413
ZZYZX <zzyzx@virtual>
2022-06-20 20:29:49 +0300

    Fix yet another XSS in CSS module (reported by Revoltage)

--- modules/css.py
+++ modules/css.py
@@ -8,5 +8,5 @@ def has_content():
 def render(_context, _params, content=''):
     print('module css')
     code = content.replace('\u00a0', ' ')
-    code = re.sub(r'<\s*/\s*style\s*>', '\\\\u003c/style\\\\u003e', code)
+    code = re.sub(r'<\s*/\s*style\s*/*\s*>', '\\\\u003c/style\\\\u003e', code)
     return '<style>%s</style>' % code
