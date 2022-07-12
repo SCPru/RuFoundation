894202dbab1daf74f7c806444702a16d86fe788e
ZZYZX <zzyzx@virtual>
2022-07-12 04:59:28 +0300

    Fix user avatar hover

--- web/js/util/user-view.tsx
+++ web/js/util/user-view.tsx
@@ -12,18 +12,18 @@ class UserView extends Component<Props> {
     render() {
         const { data } = this.props;
         if (data.type === 'system') {
-            return <span className="printuser"><strong>system</strong></span>
+            return <span className="printuser avatarhover"><strong>system</strong></span>
         }
         if (data.type === 'anonymous') {
             return (
-                <span className="printuser">
+                <span className="printuser avatarhover">
                     {data.showAvatar&&<span><img className="small" src="/-/static/images/anon_avatar.png" alt="Anonymous User" /></span>}
                     <span>Anonymous User</span>
                 </span>
             )
         }
         return (
-            <span className="printuser" data-user-name={data.username}>
+            <span className="printuser avatarhover" data-user-name={data.username}>
                 {data.showAvatar&&<a href={`/-/users/${encodeURIComponent(data.username)}`}><img className="small" src={data.avatar} alt={data.name} /></a>}
                 <a href={`/-/users/${encodeURIComponent(data.username)}`}>{data.name}</a>
             </span>
