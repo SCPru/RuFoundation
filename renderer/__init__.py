d833e4f0afaa345a022b705b3066f19593f9ccfa
ZZYZX <zzyzx@virtual>
2022-07-28 23:46:19 +0300

    Add logging

--- renderer/__init__.py
+++ renderer/__init__.py
@@ -3,19 +3,24 @@ from .parser import Parser, ParseResult, ParseContext
 from .tokenizer import StaticTokenizer
 from .nodes import Node
 import logging
+import time
 
 
 def get_cached_or_parse_article(version: ArticleVersion):
     if version.ast:
         try:
             root_node = Node.from_json(version.ast)
+            logging.info('returning existing AST for %s', version.article.full_name)
             return ParseResult(ParseContext(None, root_node), root_node)
         except (TypeError, ValueError):
             pass
+    t = time.time()
     p = Parser(StaticTokenizer(version.source))
     result = p.parse()
     version.ast = result.root.to_json()
     version.save()
+    t2 = time.time()
+    logging.info('parsing of new AST for %s took %.2fs', version.article.full_name, t2-t)
     return result
 
 
