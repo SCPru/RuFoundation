b5c5ab586de136f957674cfb6b572f1d07d8139b
MrNereof <mrnereof@gmail.com>
2022-10-29 09:35:45 +0300

    Fix css module without show=true

--- modules/css.py
+++ modules/css.py
@@ -7,5 +7,5 @@ def has_content():
 
 def render(_context, _params, content=''):
     code = content.replace('\u00a0', ' ').replace('<', '\\u003c')
-    codeblock = '<div class="code w-code language-css">{{code|safe}}</div>' if _params["show"] == "true" else ""
+    codeblock = '<div class="code w-code language-css">{{code|safe}}</div>' if _params.get("show") == "true" else ""
     return render_template_from_string('<style>{{code|safe}}</style>' + codeblock, code=code)
