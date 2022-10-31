e6c186dfe2aaf46454b48c622bb51cab7f800344
ZZYZX <zzyzx@virtual>
2022-10-31 20:12:35 +0200

    Disable module CSS show=true

--- modules/css.py
+++ modules/css.py
@@ -5,7 +5,18 @@ def has_content():
     return True
 
 
-def render(_context, _params, content=''):
+def render(_context, params, content=''):
     code = content.replace('\u00a0', ' ').replace('<', '\\u003c')
-    codeblock = '<div class="code w-code language-css">{{code|safe}}</div>' if _params.get("show") == "true" else ""
-    return render_template_from_string('<style>{{code|safe}}</style>' + codeblock, code=code)
+    # once we have a way to properly parse params, uncomment this and remove show=False
+    # show = params.get('show') in ['yes', 'true']
+    show = False
+    return render_template_from_string(
+        """
+        <style>{{code|safe}}</style>
+        {% if show %}
+        <div class="code w-code language-css">{{code|safe}}</div>
+        {% endif %}
+        """,
+        code=code,
+        show=show
+    )
