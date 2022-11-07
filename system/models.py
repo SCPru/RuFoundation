904c231df8cc59962537cddf1e78af80ea0f6b9d
MrNereof <mrnereof@gmail.com>
2022-11-07 15:45:25 +0300

    Add bots support and authentication using api keys for them

--- system/models.py
+++ system/models.py
@@ -1,4 +1,5 @@
 from django.contrib.postgres.fields import CITextField
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 from django.db import models
@@ -13,6 +14,7 @@ class User(AbstractUser):
         Normal = 'normal'
         Wikidot = 'wikidot'
         System = 'system'
+        Bot = 'bot'
 
     username = CITextField(
         max_length=150, validators=[AbstractUser.username_validator], unique=True,
@@ -29,6 +31,8 @@ class User(AbstractUser):
     avatar = models.ImageField(null=True, blank=True, upload_to='-/users', verbose_name="Аватар")
     bio = models.TextField(blank=True, verbose_name="Описание")
 
+    api_key = models.CharField(unique=True, blank=True, null=True, max_length=67, verbose_name="Апи-ключ")
+
     def get_avatar(self, default=None):
         if self.avatar:
             return '%s%s' % (settings.MEDIA_URL, self.avatar)
@@ -37,7 +41,18 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+    def _generate_apikey(self, commit=True):
+        self.password = ""
+        self.api_key = make_password(self.username)[21:]
+        if commit:
+            self.save()
+
     def save(self, *args, **kwargs):
         if not self.wikidot_username:
             self.wikidot_username = None
+        if self.type == "bot":
+            if not self.api_key:
+                self._generate_apikey(commit=False)
+        else:
+            self.api_key = None
         return super().save(*args, **kwargs)
