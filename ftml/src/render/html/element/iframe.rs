e5266f59353b9ab339eb000698bb1d3a5f575647
ZZYZX <zzyzx@virtual>
2023-02-26 21:03:48 +0200

    Allow going fullscreen from iframe

--- ftml/src/render/html/element/iframe.rs
+++ ftml/src/render/html/element/iframe.rs
@@ -27,6 +27,8 @@ pub fn render_iframe(ctx: &mut HtmlContext, url: &str, attributes: &AttributeMap
     ctx.html().iframe().attr(attr!(
         "src" => url,
         "sandbox" => "allow-scripts allow-top-navigation allow-popups allow-modals",
+        "allow" => "fullscreen",
+        "allowfullscreen" => "allowfullscreen",
         "crossorigin";;
         attributes
     ));
@@ -65,6 +67,8 @@ pub fn render_html(ctx: &mut HtmlContext, contents: &str) {
             "id" => &id,
             "srcdoc" => &format!("{prepended_script}{contents}"),
             "sandbox" => "allow-scripts allow-top-navigation allow-popups allow-modals",
+            "allow" => "fullscreen",
+            "allowfullscreen" => "allowfullscreen",
             "style" => "width: 100%; height: 0",
             "class" => "w-iframe-autoresize",
             "frameborder" => "0",
