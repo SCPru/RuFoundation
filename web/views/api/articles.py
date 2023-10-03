5af39468a5cf8d597f6df2f744dd7774f89d05d5
ZZYZX <zzyzx@virtual>
2023-10-03 04:23:11 +0300

    Fix Cerberus.aic

--- web/views/api/articles.py
+++ web/views/api/articles.py
@@ -146,7 +146,7 @@ class FetchOrRevertLogView(APIView):
         try:
             c_from = int(request.GET.get('from', '0'))
             c_to = int(request.GET.get('to', '25'))
-            get_all = request.GET.get('all') == True
+            get_all = bool(request.GET.get('all'))
         except ValueError:
             raise APIError('Некорректное указание ограничений списка', 400)
 
