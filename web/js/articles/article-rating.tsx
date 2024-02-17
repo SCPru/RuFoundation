633b96f12ed184875bff2452c96f96f1b1fb6a9e
ZZYZX <zzyzx@virtual>
2024-02-17 12:14:41 +0200

    Removed rounding in stars rating distribution

--- web/js/articles/article-rating.tsx
+++ web/js/articles/article-rating.tsx
@@ -146,6 +146,8 @@ class ArticleRating extends Component<Props, State> {
 
         votesCount[0] = votes?.length - votesCount[1];
 
+        const maxCount = Math.max(...votesCount);
+
         return (
             <div className="w-rate-dist">
                 { !!(votesCount && votesCount.length) }
@@ -153,11 +155,11 @@ class ArticleRating extends Component<Props, State> {
                     <div className="w-rate-row">
                         <div className="w-rate-num">{ i ? '-' : '+' }</div>
                         <div className="w-rate-bar">
-                            <div className="w-bar-fill" style={{ width: `${votesCount[1-i] * 100 / votes.length}%` }}></div>
+                            <div className="w-bar-fill" style={{ width: `${maxCount > 0 ? votesCount[1-i] * 100 / votes.length : 0}%` }}></div>
                         </div>
                         <div className="w-rate-stat">
-                            <div className="w-v-count">{ votesCount[1-i] }</div>
-                            <div>{ Math.round(votesCount[1-i] * 100 / votes.length) }%</div>
+                            <div className="w-v-count">{ maxCount > 0 ? votesCount[1-i] : '—' }</div>
+                            <div>{ maxCount > 0 ? `${Math.round(votesCount[1-i] * 100 / votes.length)}%` : '—' }</div>
                         </div>
                     </div>
                 )) }
@@ -171,8 +173,15 @@ class ArticleRating extends Component<Props, State> {
         let votesCount = [0, 0, 0, 0, 0, 0];
 
         votes?.forEach(vote => {
-            votesCount[Math.ceil(vote.value)]++;
+            let minCount = Math.floor(vote.value);
+            let maxCount = Math.ceil(vote.value);
+            let affectMin = vote.value - minCount;
+            let affectMax = 1.0 - (minCount + 1.0 - vote.value);
+            votesCount[minCount] += affectMin;
+            votesCount[maxCount] += affectMax;
         })
+
+        const maxCount = Math.ceil(Math.max(...votesCount));
 
         return (
             <div className="w-rate-dist">
@@ -181,11 +190,10 @@ class ArticleRating extends Component<Props, State> {
                     <div className="w-rate-row">
                         <div className="w-rate-num w-afterstar">{ 5 - i }</div>
                         <div className="w-rate-bar">
-                            <div className="w-bar-fill" style={{ width: `${votesCount[5-i] * 100 / votes.length}%` }}></div>
+                            <div className="w-bar-fill" style={{ width: `${maxCount > 0 ? votesCount[5-i] * 100 / maxCount : '0'}%` }}></div>
                         </div>
                         <div className="w-rate-stat">
-                            <div className="w-v-count">{ votesCount[5-i] }</div>
-                            <div>{ Math.round(votesCount[5-i] * 100 / votes.length) }%</div>
+                            <div className="w-v-count">{ maxCount > 0 ? `${Math.round(votesCount[5-i] * 100 / votes.length)}%` : '—' }</div>
                         </div>
                     </div>
                 )) }
