2a077d3514547bad00a73145543323a5a590cff2
btomaev <btomaev34@gmail.com>
2024-12-24 15:44:49 +0300

    Move from CiTextField to TextField

---
+++ system/migrations/0018_en_ci_collation.py
@@ -0,0 +1,21 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('system', '0017_alter_user_visual_group'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql="""
+                CREATE COLLATION IF NOT EXISTS en_ci (
+                    provider = 'icu',
+                    locale = 'en-u-ks-level2',
+                    deterministic = false
+                );
+            """,
+            reverse_sql="DROP COLLATION IF EXISTS en_ci;"
+        ),
+    ]
