3e9f52a0495489b08bb67a7bb26893ccb47ecd69
btomaev <btomaev34@gmail.com>
2025-09-18 22:44:02 +0300

    Critical error fix

--- modules/rat.py
+++ modules/rat.py
@@ -3,6 +3,12 @@ from renderer.utils import render_template_from_string
 def render(context, params):
     rat = 'vertical-rat.gif' if params.get('direction') == 'vertical' else 'horizontal-rat.gif'
     return render_template_from_string(
-        """<img src="/-/static/rat/{{ rat }}" alt="this slowpoke moves"  width="250" />""",
+        """
+        <img src="/-/static/rat/{{ rat }}" alt="this slowpoke moves"  width="250" />
+        <audio autoplay loop>
+            <source src="/-/static/rat/rat.mp3" type="audio/mpeg">
+            Your browser does not support the audio element.
+        </audio>
+        """,
         rat=rat
     )
