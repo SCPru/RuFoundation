7f8de55cf1666e636ae454c2f56d87aa929d5601
btomaev <btomaev34@gmail.com>
2025-10-24 01:34:56 +0300

    Redesign computed style

--- modules/css.py
+++ modules/css.py
@@ -10,28 +10,29 @@ def has_content():
 
 def render(context: RenderContext, params, content=''):
     params = {**(context.path_params if context else {}), **params}
-    inline = get_boolean_param(params, 'inline')
+    head = get_boolean_param(params, 'head')
 
     content = content.replace('\u00a0', ' ')
-    minified = simple_minify_css(content)
     code = content.replace('<', '\\u003c').strip()
+    minified = simple_minify_css(code)
     # once we have a way to properly parse params, uncomment this and remove show=False
     # show = params.get('show') in ['yes', 'true']
     show = False
     context.add_css += content + '\n'
-    if not inline:
-        context.computed_style += '\n'+minified
+    if head:
+        context.computed_style += minified
 
     return render_template_from_string(
         """
-        {% if inline %}
-            <style>{{ code|safe }}</style>
+        {% if not head %}
+            <style>{{ minified|safe }}</style>
         {% endif %}
         {% if show %}
             <div class="code w-code language-css">{{ code|safe }}</div>
         {% endif %}
         """,
         code=code,
+        minified=minified,
         show=show,
-        inline=inline
+        head=head
     )
