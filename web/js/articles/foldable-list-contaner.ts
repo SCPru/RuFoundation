462d6abf595c5d1df648bcdf9174ca0c98442de1
DESKTOP-QSAQ30B\Mitrotsky <mitrotskiy@yandex.ru>
2026-03-27 19:34:16 +0700

    Made early exit silent (bad practice but I'm not a maintainer to say no)

--- web/js/articles/foldable-list-contaner.ts
+++ web/js/articles/foldable-list-contaner.ts
@@ -12,7 +12,7 @@ export function makeFoldableListContainer(node: HTMLElement): void {
     try {
         isDivElement(node);
     } catch (e) {
-        console.error(e);
+        // It applies only to the div elements
         return;
     }
 
